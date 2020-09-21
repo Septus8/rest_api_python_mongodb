f0f98ccd489a6c99b91192d0545a1103e54b2bd1
samuel.frey <safrweb20kds@gmail.com>
2020-09-21 17:59:55 +0200

    working REST-API first try of MongoDB Connection

---
+++ rest_api.py
@@ -0,0 +1,91 @@
+from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+
+mycol = mydb["customers"]
+print(mydb.list_collection_names())
+
+collist = mydb.list_collection_names()
+if "customers" in collist:
+  print("The collection exists.")
+
+
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+#REST GET = find by id
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_tasks(task_id):
+    task = [task for task in tasks if task["id"] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'tasks': task[0]})
+
+#REST POST = Create new
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+#REST PUT = UPDATE
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+#REST DELETE
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Gibts nichts zu holen hier'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
