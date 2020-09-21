8e83463e5dd17184ec5de2a16cbf5d7583ee0f6a
samuel.frey <safrweb20kds@gmail.com>
2020-09-21 22:08:05 +0200

    DB-Connect & REST methods added

--- rest_api.py
+++ rest_api.py
@@ -1,45 +1,40 @@
-from flask import Flask, jsonify
-from flask import abort
-from flask import make_response
-from flask import request
+from flask import Flask, jsonify, abort, make_response, request
 import pymongo
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-
-mycol = mydb["customers"]
-print(mydb.list_collection_names())
-
-collist = mydb.list_collection_names()
-if "customers" in collist:
-  print("The collection exists.")
+import json
+from bson import json_util
 
 
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["todos"]
+
+mycol = mydb["todoData"]
+
+
+mydict = { "id": 3, "title": "Titletitle", "description": "dlkfjghn" }
+
+#x = mycol.insert_one(mydict)
+
+for x in mycol.find():
+  print(x)
+
+
+#REST GET = find all
+@app.route('/todo/api/v1.0/tasks/', methods=['GET'])
+def get_tasks():
+    tasks = mycol.find()
+    json_docs = [json.dumps(doc, default=json_util.default) for doc in tasks]
+    return jsonify({"tasks": json_docs})
 
 #REST GET = find by id
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_tasks(task_id):
-    task = [task for task in tasks if task["id"] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'tasks': task[0]})
+def get_task(task_id):
+    myquery = {"id": task_id}
+    tasks = mycol.find(myquery)
+    json_docs = [json.dumps(doc, default=json_util.default) for doc in tasks]
+    return jsonify({"tasks": json_docs})
 
 #REST POST = Create new
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
@@ -58,29 +53,53 @@ def create_task():
 #REST PUT = UPDATE
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
+    print(request.json)
+    list = mycol.find({"id": task_id})
+    tasks = [json.dumps(doc, default=json_util.default) for doc in list]
+
+    if len(tasks) == 0:
+        print("------None found")
         abort(404)
     if not request.json:
+        print("------No Data")
         abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
+        #and type(request.json['title']) != unicode
+    if 'title' in request.json:
+        print("------Title Issue")
+        #abort(400)
+    if 'description' in request.json:
+        print("------Desc Issue")
+        #abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+        print("------Done Issue")
+        #abort(400)
+
+
+    #request.json.get('title', tasks[0]['title']).Title
+    print(type(tasks))
+    print(tasks)
+    print(type(tasks[0]))
+    print(tasks[0])
+
+    y = json.loads(tasks[0])
+    y["title"] =  "newTitle"
+    print(y)
+    #["description"] = "TEST"
+    #request.json.get("description", tasks[0]['description'])
+    #tasks[0]['done'] = request.json.get('done', tasks[0]['done'])
+
+    myquery = {"id": task_id}
+    newvalues = { "$set": jsonify(tasks[0])}
+
+    mycol.update_one(myquery, newvalues)
+
+    return jsonify({'task': tasks[0]})
 
 #REST DELETE
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
+    myquery = {"id": task_id}
+    mycol.delete_one(myquery)
     return jsonify({'result': True})
 
 @app.errorhandler(404)
